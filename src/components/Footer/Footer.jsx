2e246c9d19a05eff44815b74e936fcc9e49af4c2
Vasu <vasukumar962@gmail.com>
2024-03-23 11:52:46 +0530

    fixed code footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
@@ -40,7 +40,7 @@ export default function Footer() {
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/hiteshchoudhary"
+                    href="https://github.com/vasu962"
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer"
@@ -49,9 +49,12 @@ export default function Footer() {
                   </a>
                 </li>
                 <li>
-                  <Link to="/" className="hover:underline">
-                    Discord
-                  </Link>
+                  <a
+                    href="https://www.linkedin.com/in/vasukumar01/"
+                    className="hover:underline"
+                  >
+                    Linkedin
+                  </a>
                 </li>
               </ul>
             </div>
@@ -78,8 +81,8 @@ export default function Footer() {
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center">
             © 2023
-            <a href="https://hiteshchoudhary.com/" className="hover:underline">
-              hiteshchoudhary
+            <a href="https://www.linkedin.com/in/vasukumar01/" className="hover:underline">
+              vasukumar01
             </a>
             . All Rights Reserved.
           </span>
